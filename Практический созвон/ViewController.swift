99c81de19c6efb8581054349dc8dd12fb4818fe9
HristinaBuyanova <hristina-a@mail.ru>
2023-10-14 11:37:18 +0300

    Initial Commit

---
+++ Практический созвон/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Практический созвон
+//
+//  Created by Христина Буянова on 14.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
